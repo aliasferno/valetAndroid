84c238c02af1b7f8a69ef27a3141680b86e3189e
aliasferno <aliasferno@gmail.com>
2025-01-08 01:10:55 -0500

    Parcial de app de valet parking

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('db', 'db_parcial');
-    define('usuario', 'root');
-    define('clave', '');
-    define('host', 'localhost');
-
-    $mysqli = new mysqli(host, usuario, clave, db);
-?>
